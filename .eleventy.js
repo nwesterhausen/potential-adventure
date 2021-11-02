9142d6cd45edd0011e2c3964c13b837b05800a0e
Nicholas Westerhausen <nwesterhausen@ntsupply.com>
2021-11-02 09:33:10 -0400

    add mermaid js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/css/styles.css');
   eleventyConfig.addPassthroughCopy('./src/img');
+  eleventyConfig.addMarkdownHighlighter((str, language) => {
+    if (language === "mermaid") {
+      return `<pre class="mermaid">${str}</pre>`;
+    }
+    return highlighter(str, language);
+  });
+  
   return {
     dir: {
       input: 'src',
