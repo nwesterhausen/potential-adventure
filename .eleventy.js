a3301a36c8d2600fae50ce479096c2300dedcc21
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-11-02 19:14:57 -0400

    watch script for when edit css classes (JIT)

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('./src/css/styles.css');
+  // eleventyConfig.addPassthroughCopy('./src/css/styles.css');
   eleventyConfig.addPassthroughCopy('./src/img');
   // eleventyConfig.addMarkdownHighlighter((str, language) => {
   //   if (language === "mermaid") {
