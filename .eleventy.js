a9f037e120de7c4be561ec212288d8c0b564e102
Nicholas Westerhausen <2317381+nwesterhausen@users.noreply.github.com>
2021-11-03 16:57:29 -0400

    change extensions to template

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 module.exports = function(eleventyConfig) {
-  // eleventyConfig.addPassthroughCopy('./src/css/styles.css');
   eleventyConfig.addPassthroughCopy('./src/img');
   // eleventyConfig.addMarkdownHighlighter((str, language) => {
   //   if (language === "mermaid") {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: {
-    content: ['./src/**/*.html']
+    content: ['./src/**/*.{html,liquid}']
   },
   //darkMode: false, // or 'media' or 'class'
   theme: {
